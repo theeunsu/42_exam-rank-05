3a80074a82dc0acaf9eb530e3b839fef9ecca509
theeunsu <theeunsu@gmail.com>
2025-03-18 23:24:31 +0100

    cpp_module00

---
+++ cpp_module00/Warlock.hpp
@@ -0,0 +1,28 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+
+class Warlock
+{
+	private:
+		std::string _name;
+		std::string _title;
+
+		Warlock();
+		Warlock(const Warlock &other);
+		Warlock &operator=(const Warlock &other);
+
+	public:
+		Warlock(const std::string &name, const std::string &title);
+		~Warlock();
+
+		const std::string &getName() const;
+		const std::string &getTitle() const;
+
+		void setTitle(const std::string &);
+		
+		void introduce() const;
+};
+
+#endif
